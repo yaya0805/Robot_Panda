63c660ca816192c9b811f0882ea6f83a3fe07cc5
yaya0805 <scoly_840805@yahoo.com.tw>
2016-03-10 01:30:55 +0800

    Just framework

---
+++ Robot_Panda/RobotPanda.cpp
@@ -0,0 +1,179 @@
+#include <glew.h> // glew.h must be included before gl.h/glu.h/freeglut.h
+#include <freeglut.h>
+#include <cstdio>
+#include <cstdlib>
+
+#define MENU_TIMER_START 1
+#define MENU_TIMER_STOP 2
+#define MENU_EXIT 3
+
+const GLfloat tri_v1[3] = {-0.5f, -0.4f, 0.0f};
+const GLfloat tri_v2[3] = { 0.5f, -0.4f, 0.0f};
+const GLfloat tri_v3[3] = { 0.0f,  0.6f, 0.0f};
+
+GLubyte timer_cnt = 0;
+bool timer_enabled = true;
+unsigned int timer_speed = 16;
+
+// Print OpenGL context related information.
+void dumpInfo(void)
+{
+    printf("Vendor: %s\n", glGetString (GL_VENDOR));
+    printf("Renderer: %s\n", glGetString (GL_RENDERER));
+    printf("Version: %s\n", glGetString (GL_VERSION));
+    printf("GLSL: %s\n", glGetString (GL_SHADING_LANGUAGE_VERSION));
+}
+
+// GLUT callback. Called to draw the scene.
+void My_Display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glBegin(GL_TRIANGLES);
+	{
+		glColor3ub(timer_cnt, 0, 255 - timer_cnt);
+		glVertex3fv(tri_v1);
+		glColor3ub(255, timer_cnt, 255 - timer_cnt);
+		glVertex3fv(tri_v2);
+		glColor3ub(255 - timer_cnt, 0, timer_cnt);
+		glVertex3fv(tri_v3);
+	}
+	glEnd();
+
+	glutSwapBuffers();
+}
+
+void My_Reshape(int width, int height)
+{
+	glViewport(0, 0, width, height);
+
+	float viewportAspect = (float)width / (float)height;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-1 * viewportAspect, 1 * viewportAspect, -1, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+}
+
+void My_Timer(int val)
+{
+	timer_cnt++;
+	glutPostRedisplay();
+	if(timer_enabled)
+	{
+		glutTimerFunc(timer_speed, My_Timer, val);
+	}
+}
+
+void My_Mouse(int button, int state, int x, int y)
+{
+	if(state == GLUT_DOWN)
+	{
+		printf("Mouse %d is pressed at (%d, %d)\n", button, x, y);
+	}
+	else if(state == GLUT_UP)
+	{
+		printf("Mouse %d is released at (%d, %d)\n", button, x, y);
+	}
+}
+
+void My_Keyboard(unsigned char key, int x, int y)
+{
+	printf("Key %c is pressed at (%d, %d)\n", key, x, y);
+}
+
+void My_SpecialKeys(int key, int x, int y)
+{
+	switch(key)
+	{
+	case GLUT_KEY_F1:
+		printf("F1 is pressed at (%d, %d)\n", x, y);
+		break;
+	case GLUT_KEY_PAGE_UP:
+		printf("Page up is pressed at (%d, %d)\n", x, y);
+		break;
+	case GLUT_KEY_LEFT:
+		printf("Left arrow is pressed at (%d, %d)\n", x, y);
+		break;
+	default:
+		printf("Other special key is pressed at (%d, %d)\n", x, y);
+		break;
+	}
+}
+
+void My_Menu(int id)
+{
+	switch(id)
+	{
+	case MENU_TIMER_START:
+		if(!timer_enabled)
+		{
+			timer_enabled = true;
+			glutTimerFunc(timer_speed, My_Timer, 0);
+		}
+		break;
+	case MENU_TIMER_STOP:
+		timer_enabled = false;
+		break;
+	case MENU_EXIT:
+		exit(0);
+		break;
+	default:
+		break;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	// Initialize GLUT and GLEW, then create a window.
+	////////////////////
+	glutInit(&argc, argv);
+	glewInit();
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(600, 600);
+	glutCreateWindow("Quiz Framework"); // You cannot use OpenGL functions before this line; The OpenGL context must be created first by glutCreateWindow()!
+	dumpInfo();
+	////////////////////
+	
+	// Initialize OpenGL states.
+	////////////////////////
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	////////////////////////
+
+	// Create a menu and bind it to mouse right button.
+	////////////////////////////
+	int menu_main = glutCreateMenu(My_Menu);
+	int menu_timer = glutCreateMenu(My_Menu);
+
+	glutSetMenu(menu_main);
+	glutAddSubMenu("Timer", menu_timer);
+	glutAddMenuEntry("Exit", MENU_EXIT);
+
+	glutSetMenu(menu_timer);
+	glutAddMenuEntry("Start", MENU_TIMER_START);
+	glutAddMenuEntry("Stop", MENU_TIMER_STOP);
+
+	glutSetMenu(menu_main);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+	////////////////////////////
+
+	// Register GLUT callback functions.
+	///////////////////////////////
+	glutDisplayFunc(My_Display);
+	glutReshapeFunc(My_Reshape);
+	glutMouseFunc(My_Mouse);
+	glutKeyboardFunc(My_Keyboard);
+	glutSpecialFunc(My_SpecialKeys);
+	glutTimerFunc(timer_speed, My_Timer, 0); 
+	///////////////////////////////
+
+	// Enter main event loop.
+	//////////////
+	glutMainLoop();
+	//////////////
+	return 0;
+}
